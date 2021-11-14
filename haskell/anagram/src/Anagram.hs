322326b797504ec7e70f8cb7b33c79a25ff9e751
0x28 <12886563+0x28@users.noreply.github.com>
2021-11-14 18:33:41 +0100

    Improve solution for anagram

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,16 +1,18 @@
 module Anagram (anagramsFor) where
 
-import Data.List
 import Data.Char
 
-normalize :: String -> String
-normalize = sort . map toLower
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+charCount :: String -> Map Char Int
+charCount = foldr (\c s -> Map.insertWith (+) (toLower c) 1 s) Map.empty
 
 downcase :: String -> String
 downcase = map toLower
 
 anagramsFor :: String -> [String] -> [String]
 anagramsFor xs xss =
-  let normal = normalize xs
+  let count = charCount xs
   in
-    filter (\s -> normalize s == normal && downcase s /= downcase xs) xss
+    filter (\s -> charCount s == count && downcase s /= downcase xs) xss
