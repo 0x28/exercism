1f8ad755a242b039867a0fc92ad2afccd84cce18
0x28 <12886563+0x28@users.noreply.github.com>
2022-05-08 21:31:19 +0200

    Use fromEnum in resistor-color-duo

--- haskell/resistor-color-duo/src/ResistorColors.hs
+++ haskell/resistor-color-duo/src/ResistorColors.hs
@@ -14,16 +14,4 @@ data Color
   deriving (Eq, Show, Enum, Bounded)
 
 value :: (Color, Color) -> Int
-value (a, b) = decode a * 10 + decode b
-
-decode :: Color -> Int
-decode Black = 0
-decode Brown = 1
-decode Red = 2
-decode Orange = 3
-decode Yellow = 4
-decode Green = 5
-decode Blue = 6
-decode Violet = 7
-decode Grey = 8
-decode White = 9
+value (a, b) = fromEnum a * 10 + fromEnum b
