6a07fc7d40824689a3272a68ee5d45115f9b49c5
0x28 <12886563+0x28@users.noreply.github.com>
2023-03-04 23:09:24 +0100

    Update zig resistor-color

--- zig/resistor-color/resistor_color.zig
+++ zig/resistor-color/resistor_color.zig
@@ -1,6 +1,6 @@
 pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
 
-pub fn colorCode(color: ColorBand) isize {
+pub fn colorCode(color: ColorBand) usize {
     return @enumToInt(color);
 }
 
