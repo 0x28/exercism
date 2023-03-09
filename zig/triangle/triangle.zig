e64f7627cd5e829f6e7c475cb8bc62d1136bad01
0x28 <12886563+0x28@users.noreply.github.com>
2023-03-09 17:52:42 +0100

    Add solution for zig triangle

---
+++ zig/triangle/triangle.zig
@@ -0,0 +1,34 @@
+pub const TriangleError = error{
+    Degenerate,
+    InvalidInequality,
+};
+
+pub const Triangle = struct {
+    a: f64,
+    b: f64,
+    c: f64,
+
+    pub fn init(a: f64, b: f64, c: f64) TriangleError!Triangle {
+        if (a == 0 or b == 0 or c == 0) {
+            return error.Degenerate;
+        }
+
+        if (a + b < c or a + c < b or b + c < a) {
+            return error.InvalidInequality;
+        }
+
+        return Triangle{ .a = a, .b = b, .c = c };
+    }
+
+    pub fn isEquilateral(self: Triangle) bool {
+        return self.a == self.b and self.b == self.c;
+    }
+
+    pub fn isIsosceles(self: Triangle) bool {
+        return self.a == self.b or self.b == self.c or self.a == self.c;
+    }
+
+    pub fn isScalene(self: Triangle) bool {
+        return self.a != self.b and self.b != self.c and self.c != self.a;
+    }
+};
